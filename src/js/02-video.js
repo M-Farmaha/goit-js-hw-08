9176e40ebb6665cdac79bf29410ade29ca533cdf
M-Farmaha <115003821+M-Farmaha@users.noreply.github.com>
2023-03-11 17:18:44 +0200

    task 2 completed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const onPlay = function (e) {
+  localStorage.setItem('videoplayer-current-time', e.seconds);
+};
+const onPlayThrottled = throttle(onPlay, 1000);
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+player.on('timeupdate', onPlayThrottled);
