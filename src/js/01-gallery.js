274815cdea913aae22bc9bdb9dbe06752ebf8db7
M-Farmaha <115003821+M-Farmaha@users.noreply.github.com>
2023-03-11 15:01:14 +0200

    task 1 complete

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
 // Change code below this line
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+const galleryEL = document.querySelector('.gallery');
+const GalleryImagesMarkup = createGalleryImagesMarkup(galleryItems);
+
+galleryEL.insertAdjacentHTML('beforeend', GalleryImagesMarkup);
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function createGalleryImagesMarkup(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}"/>
+</a>`;
+    })
+    .join('');
+}
